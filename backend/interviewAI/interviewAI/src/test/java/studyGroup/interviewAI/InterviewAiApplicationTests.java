00416b99111d25451ef6489d7c63ee2d08b8d2f9
soutabucks <jagsoung@daum.net>
2025-07-23 16:03:22 +0900

    Add baseBackendFile

---
+++ backend/interviewAI/interviewAI/src/test/java/studyGroup/interviewAI/InterviewAiApplicationTests.java
@@ -0,0 +1,13 @@
+package studyGroup.interviewAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
